d08046ba63fd0a59511dcc1395120764e16bdedf
NeozonS <www.sokol96@gmail.com>
2024-12-10 17:05:34 +0300

    iter8

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"github.com/NeozonS/go-shortener-ya.git/internal/handlers"
+	"github.com/NeozonS/go-shortener-ya.git/internal/middleware"
 	"github.com/NeozonS/go-shortener-ya.git/internal/server"
 	"github.com/NeozonS/go-shortener-ya.git/internal/storage"
 	"github.com/NeozonS/go-shortener-ya.git/internal/storage/file"
 	"github.com/NeozonS/go-shortener-ya.git/internal/storage/mapbd"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config := server.NewConfig()
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 
 	repositories := choiseStorage(config.FileStorage)
 	handler := handlers.NewHandlers(repositories, config)
+
+	r := chi.NewRouter()
+	r.Use(middleware.GzipRequestMiddleware)
+	r.Use(middleware.GzipResponseMiddleware)
+	r.Use(chiMiddleware.Logger)
+	r.Use(chiMiddleware.Recoverer)
 	r.Route("/", func(r chi.Router) {
 		r.Post("/api/shorten", handler.PostAPI)
 		r.Post("/", handler.PostHandler)
