05bcb48464abfbf95520bcdf95aee9a102d61012
NeozonS <www.sokol96@gmail.com>
2025-01-18 18:28:20 +0300

    iter9

--- internal/storage/file/filestorage.go
+++ internal/storage/file/filestorage.go
@@ -17,12 +17,13 @@ type UserURL struct {
 }
 
 func (m *Storage) GetURL(shortURL string) (string, error) {
-	_, err := m.file.Seek(0, 0)
+	file, err := os.Open(m.file.Name())
 	if err != nil {
 		return "", err
 	}
+	defer file.Close()
 
-	decoder := json.NewDecoder(m.file)
+	decoder := json.NewDecoder(file)
 	for {
 		var pair models.LinkPair
 		err := decoder.Decode(&pair)
@@ -39,12 +40,13 @@ func (m *Storage) GetURL(shortURL string) (string, error) {
 	return "", errors.New("url not found")
 }
 func (m *Storage) GetAllURL(userID string) ([]models.LinkPair, error) {
-	_, err := m.file.Seek(0, 0)
+	file, err := os.Open(m.file.Name())
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close()
 
-	decoder := json.NewDecoder(m.file)
+	decoder := json.NewDecoder(file)
 	var result []models.LinkPair
 	for {
 		var pair UserURL
@@ -66,21 +68,21 @@ func (m *Storage) GetAllURL(userID string) ([]models.LinkPair, error) {
 }
 
 func (m *Storage) UpdateURL(userID, shortURL, originalURL string) error {
-	_, err := m.file.Seek(0, 2)
+	file, err := os.OpenFile(m.file.Name(), os.O_WRONLY|os.O_APPEND, 0777)
 	if err != nil {
 		return err
 	}
-
+	defer file.Close()
 	pair := models.LinkPair{ShortURL: shortURL, LongURL: originalURL}
 	user := UserURL{UserID: userID, Links: []models.LinkPair{pair}}
 
-	encoder := json.NewEncoder(m.file)
+	encoder := json.NewEncoder(file)
 	return encoder.Encode(&user)
 }
 
 func NewFileStorage(filename string) *Storage {
 	//file, err := os.Create(filename)
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return nil
 	}
