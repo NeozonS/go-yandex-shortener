692c494368e27574775f4590db9c1a2e9b2f61c4
NeozonS <www.sokol96@gmail.com>
2025-03-26 22:19:34 +0300

    iter13

--- internal/storage/models/errors.go
+++ internal/storage/models/errors.go
@@ -1,14 +1,9 @@
 package models
 
 type ErrURLConflict struct {
-	ExistingURL string
-	Err         error
+	ExistingToken string
 }
 
-func (e *ErrURLConflict) Unwrap() error {
-	return e.Err
-}
-
-func (e *ErrURLConflict) Error() string {
+func (e ErrURLConflict) Error() string {
 	return "URL already exists"
 }
